26308a836561046907094b275e53314962f67e7d
Soong_Mac <840147322@qq.com>
2017-03-11 15:22:14 +0800

    create database(province city county)

---
+++ app/src/main/java/com/example/mac_soong/weather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.mac_soong.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by mac_soong on 2017/3/11.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int weatherId;
+    private int cityId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+}
